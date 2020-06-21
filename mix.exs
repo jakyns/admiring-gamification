c131e63e1c625d13992f2f6dd0cedfabb1b73bcb
teerawat1992 <teerawat.lamanchart@gmail.com>
2020-06-21 13:32:39 +0700

    Set up code coverage

--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule AG.MixProject do
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -41,7 +48,8 @@ defmodule AG.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
